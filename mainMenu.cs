fe1b3102c41e29bd6c6c376031266c640dcf774e
Yoshi-tech <122318746+Yoshi-tech@users.noreply.github.com>
2024-09-29 16:16:46 -0400

    Created main menu button script

---
+++ mainMenu.cs
@@ -0,0 +1,10 @@
+unsing System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+pubclic class playGame :  MonoBehaviour{
+  public void StartGame(){
+    SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1); // remember to go to file -> build settings and order your scenes
+  }
+}
