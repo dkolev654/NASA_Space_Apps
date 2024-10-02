8c71ff960f05d7bc7ca86736c960c3740106a224
Yoshi-tech <122318746+Yoshi-tech@users.noreply.github.com>
2024-10-02 16:44:44 -0400

    Updated clickCheck.cs in order to display info panel on click

--- clickCheck.cs
+++ clickCheck.cs
@@ -1,9 +1,29 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI; // For handling UI elements
 
-public class clickCheck: MonoBehaviour{
-  void OnMouseDown(){
-    Destroy(gameObject);
-  }
+public class Entity : MonoBehaviour
+{
+    public GameObject infoPanel; // Assign your info panel here
+
+    void Start()
+    {
+        // Initially hide the panel
+        infoPanel.SetActive(false);
+    }
+
+    void Update()
+    {
+        // If the player presses Esc, hide the panel
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            infoPanel.SetActive(false);
+        }
+    }
+
+    // When the player clicks on the entity
+    void OnMouseDown()
+    {
+        // Show the information panel
+        infoPanel.SetActive(true);
+    }
 }
